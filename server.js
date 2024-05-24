15cff0790d449b5b8a0e89dcaee03aeb793c7612
PolinaKuzmuk <polinka2082@gmail.com>
2024-05-24 14:41:21 +0200

    Feature: added authetication

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const Bcrypt = require("bcrypt");
 
 const BASE_URL = "https://664708a751e227f23ab0ca7d.mockapi.io/";
 
+const users = {
+  john: {
+    username: "john",
+    password: "$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm", // 'secret',
+    name: "John Doe",
+    id: "2133d32a",
+  },
+};
+
+const validate = async (request, username, password) => {
+  const user = users[username];
+  if (!user) {
+    return { credentials: null, isValid: false };
+  }
+
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, name: user.name };
+
+  return { isValid, credentials };
+};
+
 function getSportData(endpoint) {
-  return fetch(`${BASE_URL}${endpoint}`).then((responce) => responce.json());
+  return fetch(`${BASE_URL}${endpoint}`).then((response) => response.json());
 }
 
 const init = async () => {
@@ -13,6 +35,10 @@ const init = async () => {
     port: 3300,
     host: "127.0.0.1",
   });
+
+  await server.register(require("@hapi/basic"));
+
+  server.auth.strategy("simple", "basic", { validate });
 
   server.route({
     method: "GET",
@@ -34,79 +60,111 @@ const init = async () => {
     method: "GET",
     path: "/sports/{id}",
     handler: async (request, h) => {
-      return await getSportData("matches").then((responce) =>
-        responce.filter((item) => item.sport_id === request.params.id)
+      return await getSportData("matches").then((response) =>
+        response.filter((item) => item.sport_id === request.params.id)
       );
-    },
-  });
-
-  // server.route({
-  //   method: "DELETE",
-  //   path: "/sports/{id}",
-  //   handler: async (request, h) => {
-  //     return await fetch(`${BASE_URL}matches/${request.params.id}`, {
-  //       method: "DELETE",
-  //     }).then(() => "Element was deleted successfully");
-  //   },
-  // });
-
-  server.route({
-    method: "POST",
-    path: "/submit",
-    handler: async (request, h) => {
-      const newMatch = request.payload;
-      return await fetch(`${BASE_URL}matches`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newMatch),
-      }).then((response) => response.json());
     },
   });
 
   server.route({
     method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return `
-            <html>
-                <head>
-                    <title>Form Submission</title>
-                </head>
-                <body>
-                    <form id="myForm">
-                        <label for="sport_id">Sport ID:</label>
-                        <input type="number" id="sport_id" name="sport_id"><br><br>
-                        <label for="match_name">Match Name:</label>
-                        <input type="text" id="match_name" name="match_name"><br><br>
-                        <label for="match_start_date">Match Start Date:</label>
-                        <input type="date" id="match_start_date" name="match_start_date"><br><br>
-                        <label for="scores">Scores:</label>
-                        <input type="text" id="scores" name="scores"><br><br>
-                        <button type="submit">Submit</button>
-                    </form>
-                    <script>
-                        document.getElementById('myForm').addEventListener('submit', async function(event) {
-                            event.preventDefault();
-                            const formData = new FormData(event.target);
-                            const data = Object.fromEntries(formData.entries());
+    path: "/matches/{id}",
+    handler: async (request, h) => {
+      return await getSportData(`matches/${request.params.id}`);
+    },
+  });
 
-                            const response = await fetch('/submit', {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify(data)
-                            });
+  server.route({
+    method: "DELETE",
+    path: "/matches/{id}",
+    options: {
+      auth: "simple",
+    },
+    handler: async (request, h) => {
+      try {
+        await fetch(`${BASE_URL}matches/${request.params.id}`, {
+          method: "DELETE",
+        });
+        return h
+          .response({
+            message: "Element was deleted successfully",
+          })
+          .code(200);
+      } catch (error) {
+        console.log("Error deleting data", error);
+        return h.response({
+          message: "Failed to delete data",
+          error: error.message,
+        });
+      }
+    },
+  });
 
-                            const result = await response.json();
-                            console.log("result", result);
-                        });
-                    </script>
-                </body>
-            </html>
-        `;
+  server.route({
+    method: "PUT",
+    path: "/matches/{id}",
+    options: {
+      auth: "simple",
+    },
+    handler: async (request, h) => {
+      const updatedMatch = request.query;
+      try {
+        await fetch(`${BASE_URL}matches/${request.params.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedMatch),
+        });
+        return h
+          .response({
+            message: "Data updated successfully",
+            data: updatedMatch,
+          })
+          .code(200);
+      } catch (error) {
+        console.error("Error updating data:", error);
+        return h
+          .response({
+            message: "Failed to update data",
+            error: error.message,
+          })
+          .code(500);
+      }
+    },
+  });
+
+  server.route({
+    method: "POST",
+    path: "/new-match",
+    options: {
+      auth: "simple",
+    },
+    handler: async (request, h) => {
+      const newMatch = request.query;
+      try {
+        await fetch(`${BASE_URL}matches`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newMatch),
+        });
+        return h
+          .response({
+            message: "Data added successfully",
+            data: newMatch,
+          })
+          .code(200);
+      } catch (error) {
+        console.log("Error adding data", error);
+        return h
+          .response({
+            message: "Failed to add data",
+            error: error.message,
+          })
+          .code(500);
+      }
     },
   });
 
